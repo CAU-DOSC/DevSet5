8ce04527147bdc265989049e946bfb632222034e
dlsrks1021 <dlsrks1021@gmail.com>
2018-06-21 01:11:50 +0900

    Complete test & Remove Duplication

--- IntSetArr/src/com/intsetarr/test/IntSetArrTest.java
+++ IntSetArr/src/com/intsetarr/test/IntSetArrTest.java
@@ -12,30 +12,42 @@ class IntSetArrTest {
 
 	@Test
 	void test() {
-		int m = 10;
-		int maxval = 150;
+		int maxval = (int) Math.pow(10, 6);
+		int m = maxval/25;
+		long oldtime, oldtimeTotal;
 		int[] v = new int[m];
 		
-		int testMax = 0, testMin = maxval;
+		int testMax = 0, testMin = maxval, randnum;
 		
 		Random rand = new Random();
 		
+		System.out.println("Maxval: "+maxval+"  |  Maxelem: "+m+"\n");
+		
+		oldtimeTotal = System.currentTimeMillis();
+		oldtime = System.currentTimeMillis();
 		IntSetArr S = new IntSetArr(m, maxval);
-
+		System.out.println("Initailize time: "+(System.currentTimeMillis() - oldtime)+" ms");
+		
+		oldtime = System.currentTimeMillis();
 		while (S.size() < m) {
-			int randnum = rand.nextInt(maxval);
+			randnum = rand.nextInt(maxval);
 			S.insert(randnum);
 			if (testMax < randnum)
 				testMax = randnum;
 			if (testMin > randnum)
 				testMin = randnum;
 		}
+		System.out.println("Insert time: "+(System.currentTimeMillis() - oldtime)+" ms");
 		
+		oldtime = System.currentTimeMillis();
 		S.report(v);
+		System.out.println("Report time: "+(System.currentTimeMillis() - oldtime)+" ms");
 		
 		assertEquals(testMax, v[m - 1], "testMax");
 		assertEquals(testMin, v[0], "testMin");
-
+		
+		System.out.println("Total time: "+(System.currentTimeMillis() - oldtimeTotal)+" ms");
+		System.out.println("space: "+S.size() * 4+" bytes");
 	}
 
 }
