3ebd9c8a716f5bd7ff21b2b63d76250fc2c5b06c
qkrwngus1200 <id01096883446@gmail.com>
2018-06-21 17:51:41 +0900

    bitvec

--- IntSet/src/sets/IntSetBitVec.java
+++ IntSet/src/sets/IntSetBitVec.java
@@ -1,22 +1,51 @@
-package sets;
+package com.DevSet5.structure;
+
+import java.util.*;
 
 public class IntSetBitVec {
-	int maxval;
-	int maxelems;
-	public IntSetBitVec(int maxelems, int maxval){
-		this.maxval = maxval;
-		this.maxelems = maxelems;
+	private Vector<Boolean> bitvec;
+	private int max_value, max_elements;
+	private int size;
+	
+	public IntSetBitVec(int max_valuel, int max_elements) {
+		bitvec = new Vector<Boolean>();
+		this.max_value = max_value;
+		this.max_elements = max_elements;
+		this.size = 0;
+		
+		for (int i = 0; i <= max_value; i++) {
+			bitvec.add(i, false); //벡터 리스트 추가
+		}
+	}
+	
+	public int size() {
+		return this.size;
 	}
 	
 	public void insert(int element) {
-	
+		if ((size >= max_elements) | (element > max_value)) {
+			return;
+		}
+		if(bitvec.get(element)) {
+			return;
+		} 
+		else {
+			bitvec.remove(element);
+			bitvec.add(element, true);
+			size++;
+		}
+	}
+	public void report(int [] vec) {
+		int j = 0;
+		for(int i = 0; i <= max_value; i++) {
+			if(bitvec.get(i)) {
+				vec[j++] = i;
+			}
+		}
+		
+		for(int i = 0; i < vec.length; i++) {
+			//System.out.println("element >> " + vec[i]);
+		}
 	}
 	
-	public int size(){
-		return 0;
-	}
-	
-	public void report() {
-	
-	}
 }
--- IntSet/src/tests/SetBitVecTest.java
+++ IntSet/src/tests/SetBitVecTest.java
@@ -1,14 +1,63 @@
-package tests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class SetBitVecTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+package com.DevSet5.test;
+	
+	import static org.junit.Assert.*;
+	
+	import java.util.Random;
+	
+	import org.junit.Test;
+	import com.DevSet2.structure.IntSetBitVec;
+	
+	public class SetBitVecTest {
+	
+		@Test
+		public void setBitVecTest {
+			int max_value, max_elements;
+			long start, end, during_time;
+			
+			int mb = 1024 * 1024;
+			
+			Runtime run = Runtime.getRuntime();
+			
+			//Time elapsed
+			//Initialize
+			max_value = 1000000;
+			max_elements = max_value / 100;
+			
+			during_time = System.currentTimeMillis();
+			start = System.currentTimeMillis();
+			
+			
+		IntSetBitVec x = new IntSetBitVec(max_value, max_elements);
+			
+			end = System.currentTimeMillis();
+			System.out.println("Initialize : " + (end - start)/1000.0 + "sec");
+			
+			//Insert
+			start = System.currentTimeMillis();
+			
+			Random random = new Random();
+			while(x.size() < max_elements) {
+				x.insert(random.nextInt(max_value));
+			}
+			
+			end = System.currentTimeMillis();
+			System.out.println("Insert : " + (end - start)/1000.0 + "sec");
+			
+			//Insert
+			start = System.currentTimeMillis();
+			
+			int [] v = new int[max_elements];
+			x.report(v);
+			
+			end = System.currentTimeMillis();
+			System.out.println("Report : " + (end - start)/1000.0 + "sec");
+			
+			//Total
+			System.out.println("Total : " + (end - during_time)/1000.0 + "sec");
+			
+			//Space
+			System.out.println("Used Memory : " + (run.totalMemory() - run.freeMemory()) / mb);
+		}
+	
 	}
 
-}
